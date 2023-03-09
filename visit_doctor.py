ddff11ed9a386b37fc6f03019a745ee0b1effe87
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2023-03-09 14:25:58 +0100

    In col1 we describe some input filed with different data type.

--- visit_doctor.py
+++ visit_doctor.py
@@ -6,3 +6,14 @@ st.title(':male-doctor: Metting with the doctor :female-doctor:')
 
 # Two columns, one for inputs and another one for image
 col1, col2 = st.columns(2)
+
+with col1:
+    name = st.text_input('Name: ')
+    surname = st.text_input('Surname: ')
+    email = st.text_input('Email: ')
+    date_of_birth = st.date_input('Date of birth: ')
+    type_of_visit = st.selectbox(
+        'Please enter your type of visit!',
+        ('Eye Doctor', 'Dentist', 'Dermatologist', 'Primary Care Provider',)
+    )
+
